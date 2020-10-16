ce121feebbaa948dd0fe830e46c448fc9ed35d5d
yurgenMU <yurgen-95@mail.ru>
2020-10-16 18:59:25 +0300

    initial commit

---
+++ src/main/java/ru/maksem/simpletelegrambot/bot/Bot.java
@@ -0,0 +1,46 @@
+package ru.maksem.simpletelegrambot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String botUserName ;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage()
+                    .setChatId(chatId)
+                    .setText(messageText);
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
